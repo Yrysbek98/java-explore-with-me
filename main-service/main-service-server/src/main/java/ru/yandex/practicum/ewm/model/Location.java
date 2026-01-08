1e67d9d1b54c79ca736ec83ac2e070aa1a2a15aa
Yrysbek Abduvaliev <abduvalievyrysbek98@gmail.com>
2026-01-08 14:47:04 +0600

    удалил сущность Location

--- main-service/main-service-server/src/main/java/ru/yandex/practicum/ewm/model/Location.java
+++ main-service/main-service-server/src/main/java/ru/yandex/practicum/ewm/model/Location.java
@@ -6,20 +6,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Embeddable
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-@Entity
-@Table(name = "locations")
 public class Location {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "lat", nullable = false)
     private Float lat;
-
-    @Column(name = "lon", nullable = false)
     private Float lon;
 }
