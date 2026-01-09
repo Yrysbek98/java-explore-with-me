a39228197ffe7fdc7412d70f8dfbedb94865575b
Yrysbek Abduvaliev <abduvalievyrysbek98@gmail.com>
2026-01-09 14:01:37 +0600

    добавил EwmServiceApp

--- stats-service/stats-service-server/src/main/java/ru/yandex/practicum/ewm/repository/StatsRepository.java
+++ stats-service/stats-service-server/src/main/java/ru/yandex/practicum/ewm/repository/StatsRepository.java
@@ -15,7 +15,7 @@ public interface StatsRepository extends JpaRepository<Stats, Long> {
 
 
     @Query("""
-            SELECT new ru.practicum.stats.dto.ResponseStatsDto(s.app, s.uri, COUNT(DISTINCT s.ip))
+            SELECT new ru.yandex.practicum.ewm.ResponseStatsDto(s.app, s.uri, COUNT(DISTINCT s.ip))
             FROM Stats s
             WHERE s.timestamp BETWEEN :start AND :end
             AND (COALESCE(:uris, NULL) IS NULL OR s.uri IN :uris)
@@ -30,7 +30,7 @@ public interface StatsRepository extends JpaRepository<Stats, Long> {
 
 
     @Query("""
-            SELECT new ru.practicum.stats.dto.ResponseStatsDto(s.app, s.uri, COUNT(s.ip))
+            SELECT new ru.yandex.practicum.ewm.ResponseStatsDto(s.app, s.uri, COUNT(s.ip))
             FROM Stats s
             WHERE s.timestamp BETWEEN :start AND :end
             AND (COALESCE(:uris, NULL) IS NULL OR s.uri IN :uris)
