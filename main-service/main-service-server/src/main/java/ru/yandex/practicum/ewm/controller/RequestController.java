ab78b8f418061d8f259b6f77d12b97a89ba15c11
Yrysbek Abduvaliev <abduvalievyrysbek98@gmail.com>
2026-01-10 14:13:33 +0600

    codestyle ошибка в RequestController, удалил ненужный импорт

--- main-service/main-service-server/src/main/java/ru/yandex/practicum/ewm/controller/RequestController.java
+++ main-service/main-service-server/src/main/java/ru/yandex/practicum/ewm/controller/RequestController.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.ewm.controller;
 
-import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
